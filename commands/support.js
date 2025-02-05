c934ea63ed9f1dc425aad1f2e93d152ed4970c10
Appuffyt <lisaexe6@gmail.com>
2025-02-05 15:41:52 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -9,10 +9,10 @@ module.exports = {
     options: [],
     run: async (client, interaction, lang) => {
         try {
-            const supportServerLink = "https://discord.gg/xQF9f9yUEM";
+            const supportServerLink = "https://discord.gg/hAebvqCU";
             const githubLink = "https://github.com/GlaceYT";
             const replitLink = "https://replit.com/@GlaceYT";
-            const youtubeLink = "https://www.youtube.com/@GlaceYT";
+            const youtubeLink = "https://www.youtube.com/@bukkuz_bruh";
 
             const embed = new EmbedBuilder()
                 .setColor('#b300ff')
